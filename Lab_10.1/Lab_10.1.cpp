9a3655ddf31325c90674779ed5faaab1c5a5a318
Mykhailo <mykhailo.davydenko.ik.2023@lpnu.ua>
2023-12-05 11:54:25 +0200

    first commit

---
+++ Lab_10.1/Lab_10.1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int Count(const string s) {
+    int k = 0;
+    size_t pos = 0;
+    while ((pos = s.find("while", pos)) != -1)//повертає позицію першого входження символу
+    {
+        pos += 5;
+        k++;
+    }
+    return k;
+}
+
+int CountInFile(const string t) {
+    ifstream file(t);
+    if (!file.is_open()) //перевірка чи відкритий файл
+    {
+        cerr << "Error opening file." << endl;
+        return -1;
+    }
+
+    string line;
+    string content;
+    while (getline(file, line)) {
+        content += line + "\n";
+    }
+
+    file.close();
+
+    return Count(content);
+}
+
+int main() {
+    
+
+    int result = CountInFile("t.txt");
+
+    if (result != -1) {
+        cout << "File contained " << result << " groups of 'while'" << endl;
+    }
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <string>
+#include "../Lab_10.1/Lab_10.1.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+TEST_CLASS(StringCountFunctionTest)
+{
+public:
+
+    TEST_METHOD(TestCountFunction)
+    {
+
+        const string testString = "while do something while do something else while";
+
+        int result = Count(testString);
+
+        int expectedCount = 3;
+
+        Assert::AreEqual(expectedCount, result);
+    }
+};
